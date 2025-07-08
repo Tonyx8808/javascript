6ab3fa34e50292bfef51b3ede3c22382437287e1
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-08 21:56:49 +0200

    esercizio 2

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+//ESERCIZIO 1
+
 const surname = "Pallino"
 let age = 30
 
 console.log(age)
 console.log(surname);
+
+//ESERCIZIO 2
+
+let number = 5;
+console.log(number);
+
+//cambio il valore della variabile
+
+number = 10;
+console.log(number);
