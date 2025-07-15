9d9d39c4a366dd88179a9d4bde98fe0ea328cff3
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-15 21:03:33 +0200

    esercizio 17 e 18

---
+++ index3.js
@@ -0,0 +1,18 @@
+//ESERCIZIO 17
+
+const person = {
+ name: "Antonio",
+  age: 37,
+  city: "Napoli",
+};
+console.log(person);
+
+//ESERCIZIO 18
+
+const persone = {
+ name: "Antonio",
+  age: 37,
+  city: "Napoli",
+};
+console.log(persone.age);
+
