2116a08e02f1de369bcec5313984ce39cce6dc8e
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-15 09:44:43 +0200

    esercizio 11

--- index2.js
+++ index2.js
@@ -73,5 +73,19 @@ if (num > 5){
     console.log("la variabile è minore di 5");
 }
 
+//ESERCIZIO 11
 
+let score = parseInt(prompt("Inserisci il tuo punteggio:"));
+
+if (score >= 90 && score <= 100) {
+    console.log("voto ottimo");
+} else if (score >= 70 && score <= 89) {
+    console.log("voto buono");
+} else if (score >= 60 && score <= 69) {
+    console.log("voto sufficiente");
+} else if (score <= 59) {
+    console.log("voto insufficiente");
+} else {
+    console.log("Valore non valido");
+}
 
