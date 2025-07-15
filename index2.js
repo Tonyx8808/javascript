d6cef7728a5b251e7d182c3adf69414d100fb021
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-15 10:10:03 +0200

    esercizio 13 14 15 16

--- index2.js
+++ index2.js
@@ -88,26 +88,67 @@ if (num > 5){
 //} else {
    // console.log("Valore non valido");
 //50
-}
+//}
 
 //ESERCIZIO 12
 
-let score = parseInt(prompt("Inserisci il tuo punteggio:"));
+//let score = parseInt(prompt("Inserisci il tuo punteggio:"));
 
-switch (true) {
-    case (score >= 90 && score <= 100):
-        console.log("voto ottimo");
-        break;
-    case (score >= 70 && score <= 89):
-        console.log("voto buono");
-        break;
-    case (score >= 60 && score <= 69):
-        console.log("voto sufficiente");
-        break;
-    case (score <= 59):
-        console.log("voto insufficiente");
-        break;
-    default:
-        console.log("Valore non valido");
+//switch (true) {
+   // case (score >= 90 && score <= 100):
+      //  console.log("voto ottimo");
+       // break;
+    //case (score >= 70 && score <= 89):
+        //console.log("voto buono");
+       // break;
+    //case (score >= 60 && score <= 69):
+       // console.log("voto sufficiente");
+       // break;
+    //case (score <= 59):
+      //  console.log("voto insufficiente");
+       // break;
+   // default:
+       // console.log("Valore non valido");
+//}
+
+//ESERCIZIO 13
+
+let nu = [10, 20, 30, 40, 50];
+console.log(nu);
+
+//ESERCIZIO 14
+let numeri = [10, 20, 30, 40, 50];
+let somma = 0;
+
+for (let i = 0; i < nu.length; i++) {
+    somma += nu[i];
 }
 
+console.log("La somma dei numeri è:", somma);
+
+//ESERCIZIO 15
+
+let numer = [10, 20, 30, 40, 50];
+let arrayInvertito = [];
+
+for (let i = numer.length - 1; i >= 0; i--) {
+    arrayInvertito.push(numer[i]);
+}
+
+console.log(arrayInvertito);
+
+
+//ESERCIZIO 16
+
+let parole = [];
+
+for (let i = 0; i < 5; i++) {
+    let parola = prompt("Inserisci una parola:");
+    parole.push(parola);
+}
+
+for (let i = 0; i < parole.length; i++) {
+    if (parole[i].length % 2 !== 0) {
+        console.log(parole[i]);
+    }
+}
