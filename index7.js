5873c54671a1be1cf97f8645c6bea97fce9cfb63
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-18 09:48:09 +0200

    all'esercizio 38 ho aggiunto this.#contatoreChiamate per incrementare le chiamate

--- index7.js
+++ index7.js
@@ -1,12 +1,14 @@
-//ESERCIZO 37
-
+//ESERCIZO 38 
 class Automobile {
     #contatoreChiamate = 0;
     constructor(modello) {
         this.modello = modello;
     }
     accendiMotore() {
-        this.#contatoreChiamate++;
+        this.#contatoreChiamate++; //Aggiungo il metodo privato #incrementaContatore() che aumenta il valore di #contatoreChiamate.
+
+
+
         console.log(`${this.modello}: Motore acceso.`);
     }
     getNumeroChiamate() {
@@ -18,3 +20,6 @@ const auto = new Automobile("Opel Corsa");
 auto.accendiMotore();
 auto.accendiMotore();
 console.log(`Il metodo accendiMotore è stato chiamato ${auto.getNumeroChiamate()} volte.`);
+
+
+
