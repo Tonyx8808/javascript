f1ba1781180cf15fee1e5cad0181dbeeb2371fd3
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-21 15:49:37 +0200

    destructuring saltando il secondo

--- index13.js
+++ index13.js
@@ -1,6 +1,15 @@
-let number = [1, 2, 3, 4];
+/*let number = [1, 2, 3, 4];
 
 let [Primo, Secondo] = number;
 
 console.log("Primo numero", Primo);
-console.log("Secondo numero", Secondo);
+console.log("Secondo numero", Secondo);*/
+
+//saltare gli elementi durante il destrutturing
+
+let Number = [1, 2, 3, 4];
+
+let [primo, ,terzo] = Number;
+
+console.log("primo numero", primo);
+console.log("terzo numero", terzo);
