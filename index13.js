62ef53e89f310d5e61ea3a80afdabda5a568ec8c
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-21 15:59:07 +0200

    destructuring con valori predefiniti

--- index13.js
+++ index13.js
@@ -7,9 +7,21 @@ console.log("Secondo numero", Secondo);*/
 
 //saltare gli elementi durante il destrutturing
 
-let Number = [1, 2, 3, 4];
+/*let Number = [1, 2, 3, 4];
 
 let [primo, ,terzo] = Number;
 
 console.log("primo numero", primo);
-console.log("terzo numero", terzo);
+console.log("terzo numero", terzo);*/
+
+//destructuring con valori predefiniti
+
+let num = [1,2,3];
+
+let [a = 0, b = 0, c = 0, d = 0, e = 0] = num
+
+console.log("a:", a); 
+console.log("b:", b); 
+console.log("c:", c); 
+console.log("d:", d); 
+console.log("e:", e);
