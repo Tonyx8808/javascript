b5e31fe2f529c11c01c0c1b1584d4211f920b21f
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-21 16:09:26 +0200

    destructuring object

---
+++ index14.js
@@ -0,0 +1,11 @@
+let  person = {
+ name: "Antonio",
+  age: 37,
+  city: "Napoli",
+};
+
+let {name: personName, age, city} = person
+
+console.log(personName);
+console.log(age);
+console.log(city);
