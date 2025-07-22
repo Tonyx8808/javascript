164d32514792532c94e04e18b31cb75cc9ff7e65
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-22 09:20:51 +0200

    Destructuring con valori predefiniti

--- index14.js
+++ index14.js
@@ -1,4 +1,4 @@
-let  person = {
+/*let  person = {
  name: "Antonio",
   age: 37,
   city: "Napoli",
@@ -8,4 +8,17 @@ let {name: personName, age, city} = person
 
 console.log(personName);
 console.log(age);
+console.log(city);*/
+
+//Destructuring con valori predefiniti
+
+let person = {
+   name: "Antonio",
+  age: 37,
+  city: "Napoli",
+}
+ let { name: personName, age, city, telefon = 3336547885} = person;
+ console.log(personName);
+console.log(age);
 console.log(city);
+console.log(telefon);
