ac7de6cd47cd1a4448103be65a92596c13559c14
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-23 10:10:45 +0200

    destructuring oggetto con rest operator

--- index16.js
+++ index16.js
@@ -7,10 +7,27 @@ colcolatore(1,5,8,10);*/
 
 //destructuring array con rest operator
 
-let number = [3, 8, 10, 50, 100];
+/*let number = [3, 8, 10, 50, 100];
 
 let [valore1, valore2, ...altriValori] = number;
 
 console.log("valore1", valore1);
 console.log("valore2", valore2);
+console.log("altriValori", altriValori);*/
+
+//destructuring oggetto con rest operator
+
+let person ={
+    nome: "Antonio",
+    età: 37,
+    città: "Napoli",
+    telefono: 354798244863,
+    email: "example@MediaList.it"
+}
+
+let {nome, età, città, ...altriValori} = person;
+
+console.log("nome", nome);
+console.log("età", età);
+console.log("città", città);
 console.log("altriValori", altriValori);
