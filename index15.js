34314de057e7bc082162c639f66c665f25268a4d
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-23 09:32:59 +0200

    unione di due array

--- index15.js
+++ index15.js
@@ -1,7 +1,24 @@
-const number = [30, 10, 1, 20];
+/*const number = [30, 10, 1, 20];
 const copiaNumber =[...number];
 
 number.push(100);
 
 console.log("Array originale", number);
-console.log("Copia array", copiaNumber);
+console.log("Copia array", copiaNumber);*/
+
+//Unione di 2 array 
+
+const number1 = [10, 20, 30, 40];
+const number2 = [50, 60, 70, 80];
+
+const number3 = [...number1, ...number2];
+
+console.log(number3);
+
+//Unione di 2 array con metodo contact
+
+/*const number3 = number1.concat(number2);
+
+console.log(number3);
+console.log(number1);
+console.log(number2);*/
