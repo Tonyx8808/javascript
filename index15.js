77850cf3edb0932e741f3f7ead396a615d9de7ca
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-23 09:38:12 +0200

    copia di un oggetto con proprietà aggiuntive

--- index15.js
+++ index15.js
@@ -17,8 +17,24 @@ console.log(number3);
 
 //Unione di 2 array con metodo contact
 
-/*const number3 = number1.concat(number2);
+/*const number1 = [10, 20, 30, 40];
+const number2 = [50, 60, 70, 80];
+
+const number3 = number1.concat(number2);
 
 console.log(number3);
 console.log(number1);
 console.log(number2);*/
+
+let person = {
+ name: "Antonio",
+  age: 37,
+}
+
+let personCopia = {
+    ...person,
+    professione: "deloveper"
+};
+
+console.log("originale", person);
+console.log("copia", personCopia);
