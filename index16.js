7921c61cde6b57fc30f95c61d4c64d96d112f208
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-23 09:51:22 +0200

    rest operator

---
+++ index16.js
@@ -0,0 +1,6 @@
+function colcolatore(valore1, valore2, ...altriValori) {
+console.log("valore1", valore1)
+console.log("valore2", valore2)
+console.log("altri valori", altriValori)
+}
+colcolatore(1,5,8,10);
