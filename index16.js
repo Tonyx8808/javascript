2ec9ecbcaac46a8813da4d6eb9d95cd9a4645786
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-23 09:59:38 +0200

    destructuring array con rest operator

--- index16.js
+++ index16.js
@@ -1,6 +1,16 @@
-function colcolatore(valore1, valore2, ...altriValori) {
+/*function colcolatore(valore1, valore2, ...altriValori) {
 console.log("valore1", valore1)
 console.log("valore2", valore2)
 console.log("altri valori", altriValori)
 }
-colcolatore(1,5,8,10);
+colcolatore(1,5,8,10);*/
+
+//destructuring array con rest operator
+
+let number = [3, 8, 10, 50, 100];
+
+let [valore1, valore2, ...altriValori] = number;
+
+console.log("valore1", valore1);
+console.log("valore2", valore2);
+console.log("altriValori", altriValori);
