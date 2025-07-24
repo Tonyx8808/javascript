bb663db8b51642f24dcccb083e121c50fa95f160
Antonio Russo <antoniorusso1988@outlook.it>
2025-07-24 10:50:38 +0200

    CONVERSIONE DI UNA STRINGA JSON IN UN OGGETTO e /MANIPOLAZIONE DI DATI JSON

--- index16.js
+++ index16.js
@@ -1,4 +1,4 @@
-//Funzione che somma un numero variabile 
+/*Funzione che somma un numero variabile 
 function somma (...numeri){
     return numeri.reduce((acc, curr) => acc + curr, 0);
     }
@@ -24,3 +24,45 @@ console.log("nome", nome);
 console.log("età", età);
 console.log("città", città);
 console.log("altriValori", altriValori);*/
+
+//CONVERSIONE DI UN OGGETTO IN UNA STRINGA JSON
+
+/*const utente = {
+    nome:"Alice",
+    cognome:"Rossi",
+    età:25,
+    telefono:3245866694,
+    email:"example@mail.it",
+    attiva: true
+};
+
+const json = JSON.stringify(utente);
+console.log(json);*/
+
+//CONVERSIONE DI UNA STRINGA JSON IN UN OGGETTO
+
+/*const jsonString = '{"nome":"Alice","cognome":"Rossi","età":25,"telefono":3245866694,"email":"example@mail.it","attiva":true}';
+const ogetto = JSON.parse(jsonString);
+console.log(ogetto.nome);
+console.log(ogetto.cognome);
+console.log(ogetto.età);
+console.log(ogetto.telefono);
+console.log(ogetto.email);
+console.log(ogetto.attivac);*/
+
+//MANIPOLAZIONE DI DATI JSON
+
+const utentiJSON = `[
+    {"nome": "Alice", "età": 25},
+    {"nome": "Bob", "età": 30},
+    {"nome": "Giovani", "età": 40}
+]`;
+
+const utenti = JSON.parse(utentiJSON);
+
+const nuovoUtente = {nome: "Luca", età: 35};
+utenti.push(nuovoUtente);
+
+const utentiAggiornatiJSON = JSON.stringify(utenti, null, 2);
+
+console.log(utentiAggiornatiJSON);
