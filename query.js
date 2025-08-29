11c1dffd0e789a850e3d3a2f5bc128c9e6c2ab32
Antonio Russo <antoniorusso1988@outlook.it>
2025-08-29 16:58:37 +0200

    aggiunto innerText

--- query.js
+++ query.js
@@ -15,3 +15,21 @@ titolo.textContent = "Titolo cambiato con JS!";
 titolo.style.color = "green";
     titolo.style.transition = "color 0.3s ease";
 });
+
+const p = document.querySelector("#paragrafo");
+const btnLeggi = document.querySelector("#leggi");
+const btnCambia = document.querySelector("#cambia");
+
+//testo visibile
+
+btnCambia.addEventListener("click", () => {
+    alert(p.innerText); //mostra: "Questo è un testo in grassetto"
+});
+
+//Cambia testo
+
+btnCambia.addEventListener("click", () => {
+    paragrafo.innerText = "Testo modificato con innerText";
+    paragrafo.style.fontWeight = "bold";
+    paragrafo.style.color = "red";
+});
